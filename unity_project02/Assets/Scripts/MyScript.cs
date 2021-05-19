17276ea37b51de3bd72caa33f0b4069c98ea6baf
KimJyun <jhkim311@naver.com>
2021-05-19 12:07:10 +0900

    project02 커밋&푸시

---
+++ unity_project02/Assets/Scripts/MyScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    public GameObject MyCube1;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        MyCube1.transform.Rotate(0, -0.5f, 0);
+
+    }
+}
